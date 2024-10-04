4287f73d18f01014f441d73999068a2b2b6b3052
usama <usamanazir13@gmail.com>
2024-10-03 23:26:10 -0700

    adjusted alignment text

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -8,9 +8,8 @@ const Homepage = () => {
         <h1 className="text-7xl md:text-7xl lg:text-clamp text-center md:text-right">Saeed Junior</h1>
         <div className='text-center md:text-right'>
           <h2 className="text-lg md:text-xl mini-text">Based in Dubai</h2>
-          <p className='text-lg md:text-xl flex gap-2 justify-center items-center mini-text text-center md:text-right'>
-            <span className='w-4 h-4 rounded-full bg-[#94F77C] animate-pulse'></span>Available for a full-time position</p>
-        </div>
+            <div className='mini-text text-center md:text-right justify-center items-center'><span className='animate-pulse'>🟢 </span> Available for a full-time position</div>
+          </div>
       </div>
 
       {/* Middle Section (Image with Animated Gradient Circle) */}
