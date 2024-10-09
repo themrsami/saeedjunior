f6875bf965f17553892d1a35ff75a4e3e30e731a
usama <usamanazir13@gmail.com>
2024-10-09 01:22:47 -0700

    adjusted heading

--- app/components/Skillsheading.js
+++ app/components/Skillsheading.js
@@ -2,35 +2,29 @@ import React from 'react'
 
 const Skillsheading = () => {
   return (
-    <>
     <div className="divider divider--center">
-    <div
-      className="divider__line divider__line--left"
-      style={{
-        translate: "none",
-        rotate: "none",
-        scale: "none",
-        transform: "translate3d(0px, 0px, 0px)"
-      }}
-    />
-    <div className="divider__text">
-      <h2 data-anim="from-bottom">
-        Discover my creative expertise
-      </h2>
-    </div>
-    <div
-      className="divider__line divider__line--right"
-      style={{
-        translate: "none",
-        rotate: "none",
-        scale: "none",
-        transform: "translate3d(0px, 0px, 0px)"
-      }}
-    />
+  <div
+    className="divider__line divider__line--left"
+    style={{
+      translate: "none",
+      rotate: "none",
+      scale: "none",
+      transform: "translate3d(0px, 0px, 0px)"
+    }}
+  />
+  <div className="divider__text">
+    Explore my creative expertise
   </div>
-
-</>
-
+  <div
+    className="divider__line divider__line--right"
+    style={{
+      translate: "none",
+      rotate: "none",
+      scale: "none",
+      transform: "translate3d(0px, 0px, 0px)"
+    }}
+  />
+</div>
   )
 }
 
