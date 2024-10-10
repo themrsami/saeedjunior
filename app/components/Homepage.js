95318530d0cde842fb30cab86952480ccdb7205c
usama <usamanazir13@gmail.com>
2024-10-09 23:27:19 -0700

    changed headline

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -62,7 +62,7 @@ const Homepage = () => {
     <div className="px-4 flex flex-col md:flex-row justify-center items-center h-screen text-white text-center whitespace-nowrap">
       {/* Left Side (Text Section) */}
       <div className="flex flex-col justify-center -space-y-2">
-        <h1 ref={headingRef} className="text-7xl md:text-7xl lg:text-clamp text-center md:text-right">Saeed Junior</h1>
+        <h1 ref={headingRef} className="text-7xl md:text-7xl lg:text-clamp text-center md:text-right">Saeed Ijaz</h1>
         <div ref={locationRef} className='text-center md:text-right'>
           <h2 className="text-lg md:text-xl mini-text">Based in Dubai</h2>
           <div className='mini-text text-center md:text-right justify-center items-center'>
@@ -97,8 +97,8 @@ const Homepage = () => {
       {/* Right Side (Text Section) */}
       <div ref={descriptionRef} className="flex flex-col justify-center items-center mt-4 md:mt-0 text-center md:text-left">
         <p className="text-5xl md:text-5xl lg:text-clamp md:flex md:flex-col">
-          <span>Graphics Designer </span>
-          <span>& VFX Artist</span>
+          <span>Digital Designer </span>
+          <span>& Content Producer</span>
         </p>
       </div>
     </div>
